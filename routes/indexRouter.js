653c02e0d9802dab2056175b91eb2fcf7dcc1e09
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2020-03-17 01:18:10 -0500

    Add routers to get user input and display data from API

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,15 +7,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Travel Itinerary' });
 });
 
-router.get('/flight', async function(req, res, next) {
-  try {
-    let async_data = await flight.getFlightData();
-    res.render('flight', {title: 'Flight Information', async_data});
+router.get('/flight', function(req, res, next) {
+  res.render('flight', {title: 'Flight Information'});
+});
 
-  } catch (err) {
-    res.status(err.response.status)
-    console.log("Caught error " + err.message);
-  }
+router.post('/flight', async function(req, res, next) {
+  let apiPathString = req.body.pathString;
+
+  try {
+      let async_data = await flight.getFlightData(apiPathString);
+      res.render('flight', {title: 'Flight Information', async_data});
+
+    } catch (err) {
+      res.status(err.response.status)
+      console.log("Caught error! " + err.message);
+    }
 });
 
 module.exports = router;
