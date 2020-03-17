8b3c239f7109b3fcc5b90f7d0c41f800ad91a987
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2020-03-16 22:39:23 -0500

    Add JS to accept user input and create path string

--- public/js/script.js
+++ public/js/script.js
@@ -30,3 +30,14 @@ function signOut() {
     console.log('User signed out.');
   });
 }
+
+var flightSubmitBtn = document.querySelector('#submit-flight');
+
+flightSubmitBtn.addEventListener('click', function(e) {
+  let flightNumberVal = document.querySelector('#flight-number').value;
+  let flightNumber = flightNumberVal.replace(' ', '');
+  let depDate = document.querySelector('#departure-date').value;
+
+  let requestString = '/flights/' + flightNumber + '/' + depDate + '?withLocation=false&withAircraftImage=false';
+  console.log(requestString);
+});
