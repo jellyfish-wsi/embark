64fcbf494c507a40465bc95510005c6c1181274e
Dasha Day Hisoler <dashadayhisoler@gmail.com>
2020-03-30 23:26:37 -0500

    Implement Passport JS for Google OAuth with connect-ensure-login

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,28 +1,9 @@
 var express = require('express');
-var flight = require('../lib/flight-data');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Travel Itinerary' });
-});
-
-router.get('/flight', function(req, res, next) {
-  res.render('flight', {title: 'Flight Information'});
-});
-
-router.post('/flight', async function(req, res, next) {
-  let flightNumber = req.body.flightNumber;
-  let departureDate = req.body.depDate;
-
-  try {
-      let async_data = await flight.getFlightData(flightNumber, departureDate);
-      res.render('flight', {title: 'Flight Information', async_data});
-
-    } catch (err) {
-      res.status(err.response.status)
-      console.log("Caught error! " + err.message);
-    }
 });
 
 module.exports = router;
--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+// Get configured passport from lib/google-auth.js
+var passport = require('../lib/google-auth').getConfiguredPassport();
 
-router.get('/login', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('login', { page_name: 'Login' });
+});
+
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/auth' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/flight');
+  });
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/auth');
 });
 
 module.exports = router;
