ca6ecd5f32a6493d396dabcdc3a8345df7777153
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2020-04-10 18:28:33 -0500

    Use Sequelize to write and connect to database

---
+++ db/migrations/20200410220007-create-transportation.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Transportation', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      company_name: {
+        type: Sequelize.STRING
+      },
+      mode: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Transportation');
+  }
+};
---
+++ db/models/trip.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Trip = sequelize.define('Trip', {
+    name: DataTypes.STRING,
+    start_date: DataTypes.DATEONLY,
+    return_date: DataTypes.DATEONLY,
+    transport_mode: DataTypes.STRING
+  }, {});
+  Trip.associate = function(models) {
+    // associations can be defined here
+  };
+  return Trip;
+};
