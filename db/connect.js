04cbfee54e14dac5472b22313b65fb1517061d52
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2020-05-04 17:07:09 -0500

    Change password into an env variable

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: 'process.env.DB_PASSWORD',
   database: 'db'
 });
 
