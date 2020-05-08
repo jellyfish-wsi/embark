06fea77add3ccaa636c9f65ac0942b14b654fe79
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2020-05-07 22:56:42 -0500

    Document test/test.js with JSDoc

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
+/** Requires assert of `assert` library and use strict.
+* @requires assert
+*/
 const assert = require('assert').strict;
-
+/** Requires flight-data of `flight-data` library.
+* @requires flight-data
+*/
 const jellyfish = require('../lib/flight-data.js');
 
+/** A function that tests whether the received object is as expected
+* @function
+*/
 describe('getFlightData', function() {
   it('should return a known API response object', function() {
     return jellyfish.getFlightData()
