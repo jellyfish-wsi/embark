5ce20f22684b22db0bc73d27bc0f01a0ff7295f3
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2020-05-07 22:53:45 -0500

    Create documentation for js/script.js using JSDoc

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,4 @@
+/** @function */
 function init() {
   gapi.load('auth2', function() {
     console.log('inside init');
@@ -5,25 +6,33 @@ function init() {
   });
 }
 
+/** A function that gets user data for client-side scripts.
+* @function onSignIn
+*/
 function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log("ID: " + profile.getId()); /** Don't send this directly to your server! */
   console.log('Full Name: ' + profile.getName());
   console.log('Given Name: ' + profile.getGivenName());
   console.log('Family Name: ' + profile.getFamilyName());
   console.log("Image URL: " + profile.getImageUrl());
   console.log("Email: " + profile.getEmail());
 
-  // The ID token you need to pass to your backend:
+  /** The ID token you need to pass to your backend: */
   var id_token = googleUser.getAuthResponse().id_token;
   console.log("ID Token: " + id_token);
 }
 
+/** A function that displays a failed login if event occurs.
+* @function onFailure
+*/
 function onFailure() {
   console.error('Sign in has failed!');
 }
 
+/** A function for signing out.
+* @function signOut
+*/
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
