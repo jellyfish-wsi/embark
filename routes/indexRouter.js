84e7ce6a68ba2c823ca37a3d14fca5ccd0967898
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2020-05-07 22:55:35 -0500

    Document code within routes/indexRouter.js with JSDoc

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,18 @@
+// Index router.
+
+/** Requires express of `express` library.
+* @requires express
+*/
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+/** @description GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Travel Itinerary' });
 });
 
+/** A module that connects to express.Router().
+* @module
+*/
 module.exports = router;
