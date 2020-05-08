ec4cee76fbe36b2e4352d10abf6cffdfa6808615
WANMIN ZHANG <wzhang90@hawk.iit.edu>
2020-05-07 22:55:07 -0500

    Establish documentation for routes/flightRouter.js

--- routes/flightRouter.js
+++ routes/flightRouter.js
@@ -1,20 +1,41 @@
+// Flight data router.
+
+/** Requires express of `express` library.
+* @requires express
+*/
 var express = require('express');
+/** Requires flight-data of `flight-data` library.
+* @requires flight-data
+*/
 var flight = require('../lib/flight-data');
+/** Requires connect-ensure-login of `connect-ensure-login` library.
+* @requires connect-ensure-login
+*/
 var connect = require('connect-ensure-login');
 var router = express.Router();
 
 router.get('/',
   connect.ensureLoggedIn('/auth'),
+  /** Function to get user's name.
+  * @function
+  */
   function(req, res, next) {
     res.render('flight', {title: 'Flight Information', user: req.user});
   });
 
 router.post('/',
   connect.ensureLoggedIn('/auth'),
+  /** An async function for flight number and departure date.
+  * @async
+  * @function
+  */
   async function(req, res, next) {
     let flightNumber = req.body.flightNumber;
     let departureDate = req.body.depDate;
 
+    /**
+    * @throws Will throw error on faulty response.
+    */
     try {
         let async_data = await flight.getFlightData(flightNumber, departureDate);
         res.render('flight', {title: 'Flight Information', user: req.user, async_data});
@@ -25,4 +46,7 @@ router.post('/',
       }
   });
 
+/** A module that connects to express.Router().
+* @module
+*/
 module.exports = router;
