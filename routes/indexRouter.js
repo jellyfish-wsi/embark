f737a568f1c8ad6adc88ec779f6f531deaaa4b05
Dasha Day Hisoler <dashadayhisoler@gmail.com>
2020-05-08 22:31:09 -0500

    Implement

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,11 +4,17 @@
 * @requires express
 */
 var express = require('express');
+/** Requires connect-ensure-login of `connect-ensure-login` library.
+* @requires connect-ensure-login
+*/
+var connect = require('connect-ensure-login');
 var router = express.Router();
 
 
 /** @description GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',
+  connect.ensureLoggedIn('/auth'),
+  function(req, res, next) {
   res.render('index', { title: 'Travel Itinerary' });
 });
 
